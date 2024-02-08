597432acd990bae92ecc3aeaef6b99d1454ff034
Davide-Lot <davide.lotito@studenti.unicam.it>
2024-02-08 18:18:31 +0100

    Modified classes of Users (removed getters and setters), implimented class Post and created classes Image and ImageList

--- src/main/java/com/nfd/progetto_ids_nfd/Model/Users/AuthorizedUser.java
+++ src/main/java/com/nfd/progetto_ids_nfd/Model/Users/AuthorizedUser.java
@@ -9,51 +9,6 @@ public class AuthorizedUser extends AuthenticatedUser {
     public AuthorizedUser(String name, String surname, String email, String password, Date registrationDate) {
         super(name, surname, email, password, registrationDate);
     }
-    
-    // Getter and setter for 'name'
-    public String getAuthorizedUserName() {
-        return super.getName();
-    }
-
-    public void setAuthorizedUserName(String name) {
-        super.setName(name);
-    }
-
-    // Getter and setter for 'surname'
-    public String getAuthorizedUserSurname() {
-        return super.getSurname();
-    }
-
-    public void setAuthorizedUserSurname(String surname) {
-        super.setSurname(surname);
-    }
-
-    // Getter and setter for 'email'
-    public String getAuthorizedUserEmail() {
-        return super.getEmail();
-    }
-
-    public void setAuthorizedUserEmail(String email) {
-        super.setEmail(email);
-    }
-
-    // Getter and setter for 'password'
-    public String getAuthorizedUserPassword() {
-        return super.getPassword();
-    }
-
-    public void setAuthorizedUserPassword(String password) {
-        super.setPassword(password);
-    }
-
-    // Getter and setter for 'registrationDate'
-    public Date getAuthorizedUserRegistrationDate() {
-        return super.getRegistrationDate();
-    }
-
-    public void setAuthorizedUserRegistrationDate(Date registrationDate) {
-        super.setRegistrationDate(registrationDate);
-    }
 
     @Override
     public void createPostRequest(){
